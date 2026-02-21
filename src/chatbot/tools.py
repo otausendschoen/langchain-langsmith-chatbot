71ef244b4cbd3e1d97cffa1cfb5882e0ba2b6ce0
Oliver <oliver.tausendschoen@yahoo.com>
2026-02-21 09:31:09 +0100

    fix: Resolve TavilySearchResults deprecation warning by using langchain-tavily

--- src/chatbot/tools.py
+++ src/chatbot/tools.py
@@ -1,9 +1,9 @@
 import os
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
 
 tools = []
 if os.getenv("TAVILY_API_KEY"):
-    tool = TavilySearchResults(max_results=2)
+    tool = TavilySearch(max_results=2) # Changed class name
     tools.append(tool)
 else:
     print("Warning: TAVILY_API_KEY not found. Web search tool will not be available.")
